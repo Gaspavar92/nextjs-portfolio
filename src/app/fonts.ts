ab0ba4d28a72b0d5186d12009db013aa97532c93
Gaspare Tortora <g.tortora2@gmail.com>
2024-10-13 16:48:08 -0400

    Added new font

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -4,6 +4,7 @@ import { Protest_Riot } from "next/font/google";
 import { Titan_One } from "next/font/google";
 import { Gloria_Hallelujah } from "next/font/google";
 import { Yatra_One } from "next/font/google";
+import { Sono } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const bangers = Bangers({weight: ["400"], subsets: ["latin"]})
@@ -11,3 +12,4 @@ export const protest = Protest_Riot({weight: ["400"], subsets: ['latin']})
 export const titan = Titan_One({weight: "400", subsets: ["latin"]});
 export const gloria = Gloria_Hallelujah({weight: "400", subsets: ["latin"]});
 export const yatra = Yatra_One({weight: "400", subsets: ["latin"]});
+export const sono = Sono({weight: "400", subsets: ["latin"]});
