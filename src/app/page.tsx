94e8a1c9780c61dcf2928a2af46bc01a1f371d32
Gaspare Tortora <g.tortora2@gmail.com>
2024-10-13 16:43:53 -0400

    Added more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 
-import gsap from "gsap"
-import Introduction from "./components/introduction";
-import Nav from "./components/navigation";
-import Portrait from "./components/portrait";
-import About from "./components/about";
+import {gsap} from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Introduction from "./components/Introduction";
+import Nav from "./components/Navigation";
+import Portrait from "./components/Portrait";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects"
 
 export default function Home() {
 
@@ -16,15 +20,17 @@ export default function Home() {
 }
 
   return (
-    <main className="bg h-screen w-screen bg-fixed bg-cover pt-10 overflow-x-hidden">
+    <main className="bg w-screen bg-fixed bg-cover pt-10 overflow-x-hidden min-h-screen">
       <Nav />
-      <div className="hero section wrapper flex h-[75vh] pt-10">
+      <div className="hero my-[100px] mx-auto max-w-[80vw] flex items-center h-[75vh]">
         <div className="h-[450px] w-full flex justify-around content-center">
           <Introduction handleClick={portraitAnimation}/>
           <Portrait handleClick={portraitAnimation}/>
         </div>
       </div>
       <About />
+      <Projects />
+      <Footer />
     </main>
   );
 }
