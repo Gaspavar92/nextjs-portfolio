a58bc7eafd98f85917c8be80c49140f44e301341
Gaspare Tortora <g.tortora2@gmail.com>
2024-10-24 23:06:19 -0400

    Added section titles and animation

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,9 +1,10 @@
-import gsap from "gsap";
+import {gsap} from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import getProjects from "../netlify-projects";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { bangers, sono } from "../fonts";
+import { bangers, sono, protest } from "../fonts";
 import { Project } from "../types";
 
 
@@ -88,6 +89,25 @@ const Projects = () => {
             ]
         }
     }
+    
+    const project = 'projects'.split('');
+
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.from('.project-letter', {
+            scrollTrigger: {
+                trigger: '.project-text',
+                toggleActions: "play none none reverse",
+                start: "top 200px"
+            },
+            translateY: 200,
+            duration: 0.4,
+            stagger: 0.1,
+            ease: "back(3)",
+            autoAlpha: 0
+        })
+    }, [loading]);
 
     useEffect(() => {
         const fetchProjects = async () => {
@@ -103,6 +123,8 @@ const Projects = () => {
         fetchProjects();
     }, [])
 
+
+
     if (loading) {
         return <div>Loading...</div>
     }
@@ -112,43 +134,47 @@ const Projects = () => {
     }
 
     return (
-        <div className="projects wrapper">
-            <h2 className="text-4xl font-bold mb-8 text-center">My Projects</h2>
-            <div className="project-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-                {projects.map((project) => (
-                    <div key={project.id} id={project.id} className="project-card h-62 rounded-lg shadow-lg overflow-hidden transform transition relative">
-                        <div className="project-image w-full h-full">
-                            <Image height={200} width={200} className="w-full project-img" src={project.screenshot_url} alt={"project screenshot"} />
-                        </div>
-                        <div className="project-info p-2 md:p-6 absolute bottom-0 flex flex-col w-full h-full justify-end">
-                            <h3 className={`${bangers.className} text-xl font-semibold mb-2`}>{descriptions[project.name] ? descriptions[project.name].name : project.name}</h3>
-                            <p className={`${sono.className} mb-4 text-xs`}>{descriptions[project.name] ? descriptions[project.name].description : "No description"}</p>
-                            <div className="flex justify-between items-center text-sm">
-                                <div className="flex space-x-2">
-                                    <a href={project.url} target="_blank" rel="noopener noreferrer" aria-label="View live project" className="flex gap-2 hover:underline text-sky-600 cursor-pointer">
-                                        <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                            <circle cx="12" cy="12" r="10" />
-                                            <line x1="2" y1="12" x2="22" y2="12" />
-                                            <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
-                                        </svg>
-                                    </a>
-                                    <a href={project.build_settings.repo_url} target="_blank" rel="noopener noreferrer" aria-label="View GitHub repository" className="flex gap-2 hover:underline text-sky-600 cursor-pointer">
-                                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                                            <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
-                                        </svg>
-                                    </a>
-                                </div>
-                                <div className="technical-stack flex items-center gap-2 md:gap-4 overflow-x-auto">
-                                    {descriptions[project.name].tech.map(iconUrl => {
-                                        return <Image key={project.name} id={project.name} className="tech-icon" src={iconUrl} height={20} width={20} alt="Tech stack icon"/>
-                                    })}
+            <>
+                <div id="projects" className={`${protest.className} project-text section-title md:h-screen h-[60vh] text-[25vw] flex items-center`}>{project.map((letter, index) => {
+                    return letter === " " ? <p key={index} className="project-letter inline-block !invisible">-</p> : <p key={index} className="project-letter inline-block">{letter}</p>
+                })}</div>
+            <div className="projects wrapper">
+                <div className="project-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    {projects.map((project) => (
+                        <div key={project.id} id={project.id} className="project-card h-62 rounded-lg shadow-lg overflow-hidden transform transition relative">
+                            <div className="project-image w-full h-full">
+                                <Image height={200} width={200} className="w-full project-img" src={project.screenshot_url} alt={"project screenshot"} />
+                            </div>
+                            <div className="project-info p-2 md:p-6 absolute bottom-0 flex flex-col w-full h-full justify-end">
+                                <h3 className={`${bangers.className} text-xl font-semibold mb-2`}>{descriptions[project.name] ? descriptions[project.name].name : project.name}</h3>
+                                <p className={`${sono.className} mb-4 text-xs`}>{descriptions[project.name] ? descriptions[project.name].description : "No description"}</p>
+                                <div className="flex justify-between items-center text-sm">
+                                    <div className="flex space-x-2">
+                                        <a href={project.url} target="_blank" rel="noopener noreferrer" aria-label="View live project" className="flex gap-2 hover:underline text-sky-600 cursor-pointer">
+                                            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                                <circle cx="12" cy="12" r="10" />
+                                                <line x1="2" y1="12" x2="22" y2="12" />
+                                                <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+                                            </svg>
+                                        </a>
+                                        <a href={project.build_settings.repo_url} target="_blank" rel="noopener noreferrer" aria-label="View GitHub repository" className="flex gap-2 hover:underline text-sky-600 cursor-pointer">
+                                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                                                <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
+                                            </svg>
+                                        </a>
+                                    </div>
+                                    <div className="technical-stack flex items-center gap-2 md:gap-4 overflow-x-auto">
+                                        {descriptions[project.name].tech.map(iconUrl => {
+                                            return <Image key={project.name} id={project.name} className="tech-icon" src={iconUrl} height={20} width={20} alt="Tech stack icon"/>
+                                        })}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     )
 };
 
--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -3,8 +3,11 @@ import {gsap} from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import TechStack from "./stack";
+import { protest } from "../fonts";
 
 export default function About() {
+
+    const about = 'about'.split('');
 
     useGSAP(() => {
         gsap.registerPlugin(useGSAP, ScrollTrigger);
@@ -14,20 +17,36 @@ export default function About() {
                 toggleActions: "play reverse play reverse",
                 start: "top 20%",
                 end: "bottom 50%",
-                markers: true
+                // markers: true
             },
             translateX: -200,
             duration: 0.7,
             ease: "back",
             autoAlpha: 0
         })
+
+        gsap.from('.about-letter', {
+            scrollTrigger: {
+                trigger: '.about-text',
+                toggleActions: "play none none reverse",
+                start: "20px 200px"
+            },
+            translateY: 200,
+            duration: 0.4,
+            stagger: 0.1,
+            ease: "back(3)",
+            autoAlpha: 0
+        })
     }, []);
 
     return(
         <>
-            <div id="about" className="about wrapper flex w-full h-full justify-between">
-                <Image src={"/Gaspare-about.png"} height={"200"} width={"200"} alt={"Gaspare's picture with his son Noah"} className="about-image rounded-lg invisible h-full w-1/3"/>
-                <div className="about-text w-1/2 self-center relative">
+            <div id="about" className={`${protest.className} about about-text section-title text-[25vw] md:h-screen h-[60vh] flex items-center w-screen`}>{about.map((letter, index) => {
+                return <p key={index} className="about-letter inline-block invisible">{letter}</p>
+            })}</div>
+            <div className="wrapper flex flex-col md:flex-row w-full h-full justify-between">
+                <Image src={"/Gaspare-about.png"} height={"200"} width={"200"} alt={"Gaspare's picture with his son Noah"} className="about-image rounded-lg invisible h-full w-full md:w-1/3"/>
+                <div className="about-text h-1/2 w-1/2 self-center relative">
                     <p>I love coding, obviously. But my life journey has provided me with a lot of experiences in different fields. My personal and professional life has been fun and full of learnings. Care to know more? Ask my chatbot.</p>
                 </div>
             </div>
